06254a0f0820cf06cd48ce6b2f4586991db08192
Nathan Smith <nategsmith.7@gmail.com>
2023-02-08 21:02:03 -0500

    Knowledge Check 1 : Nathan Smith

---
+++ assignment.py
@@ -0,0 +1,10 @@
+print("Hello World !")
+
+list = [1,'Nathan',2,'Basketball',88,101,'Baseball']
+print(list[1])
+
+dictionary = {'Nathan' : 'Basketball', 'Jim' : 'Baseball'}
+print(dictionary['Nathan'])
+
+tuple = ('Basketball', 'Baseball', 'Football', 'Soccer', 'Hockey')
+print(tuple[3])
